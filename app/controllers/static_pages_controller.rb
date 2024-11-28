eac737bfb6df85663b5bae3168550a39a0172078
Pakapol Tanachaisith <meen260600@gmail.com>
2024-11-28 15:07:54 +0700

    Build flickr form and photos gallery

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,15 @@
+require "flickr"
+
 class StaticPagesController < ApplicationController
   def home
+    flickr = Flickr.new(ENV["FLICKR_KEY"], ENV["FLICKR_SECRET"])
+    @photos = flickr.photos.search(user_id: "201903833@N06")
+    @photos = @photos.map { |photo| flickr_photo_url(photo) }
+  end
+
+  private
+
+  def flickr_photo_url(flickr_photo)
+    "https://live.staticflickr.com/#{flickr_photo.server}/#{flickr_photo.id}_#{flickr_photo.secret}.jpg"
   end
 end
