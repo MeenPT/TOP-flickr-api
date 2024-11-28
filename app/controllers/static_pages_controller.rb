e111b1929f7ede41c3fcf2601ce142c36714c943
Pakapol Tanachaisith <meen260600@gmail.com>
2024-11-28 16:10:05 +0700

    Make photos gallery dynamic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,13 @@ require "flickr"
 
 class StaticPagesController < ApplicationController
   def home
-    flickr = Flickr.new(ENV["FLICKR_KEY"], ENV["FLICKR_SECRET"])
-    @photos = flickr.photos.search(user_id: "201903833@N06")
-    @photos = @photos.map { |photo| flickr_photo_url(photo) }
+    @photos = []
+
+    if params[:flickr_id].present?
+      flickr = Flickr.new(ENV["FLICKR_KEY"], ENV["FLICKR_SECRET"])
+      @photos = flickr.photos.search(user_id: params[:flickr_id])
+      @photos = @photos.map { |photo| flickr_photo_url(photo) }
+    end
   end
 
   private
